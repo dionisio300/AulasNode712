4bc4298885ffddc0611248e74e5d18f8eee9543d
dionisio300 <dionisioassis80@gmail.com>
2025-03-08 16:00:35 -0300

    Inicialização com o express

--- Aula2/index.mjs
+++ Aula2/index.mjs
@@ -9,88 +9,115 @@ Exibir um resumo da compra.
 Os produtos disponíveis devem ser armazenados em um array:
  */
 
-import readline from 'readline'
+// import readline from 'readline'
 
 
-const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
+// const rl = readline.createInterface({
+//     input:process.stdin,
+//     output:process.stdout
+// })
+
+// function cadastrarProduto(produtos){
+//     let nome = ''
+//     let preco = 0
+//     rl.question('Digite o nome do produto\n',(resposta) =>{
+//         nome = resposta
+//         rl.question('Digite o preço do produto\n',(resposta) =>{
+
+//             preco = parseFloat(resposta)
+
+//             let novoProduto = {nome:nome, preco:preco}
+//             console.log(novoProduto)
+//             produtos.push(novoProduto)
+//             console.log(produtos)
+//         })
+//     })
+// }
+
+// function criarCarrinho(produtos,carrinho){
+//     for(let i=0;i<produtos.length;i++){
+//         console.log(`${i} - nome: ${produtos[i].nome}, preço: ${produtos[i].preco}`)
+//     }
+//     rl.question('Selecione o produto a ser adicionado\n',(resp) =>{
+//         resp = parseInt(resp)
+//         carrinho.push(produtos[resp])
+//         console.log(carrinho)
+//     })
+// }
+
+// function mostrarTotal(carrinho){
+//     let total = 0
+//     for(let i = 0; i<carrinho.length;i++){
+//         total += carrinho[i].preco
+//     }
+//     console.log(`O total do carrinho é: ${total}`)
+// }
+
+// function mostrarCompras(carrinho){
+//     console.log('Itens do carrinho:')
+//     for(let i = 0; i<carrinho.length;i++){
+//         console.log(`nome: ${carrinho[i].nome} preço: ${carrinho[i].preco}`)
+//     }   
+//     mostrarTotal(carrinho)
+// }
+
+
+// let produtos = [
+//     {nome:'Notebook', preco:2500},
+//     {nome:'Monitor', preco:600},
+//     {nome:'Memória', preco:200},
+//     {nome:'Placa de vídeo',preco:650}
+// ]
+
+// let carrinho = [
+//     {nome:'Memória', preco:200},
+//     {nome:'Monitor', preco:600}
+// ]
+
+
+// rl.question('1 - Cadastrar produto\n2 - Criar Carrinho\n3 - Mostrar total\n4 - Exibir Resumo da compra\n',(resposta)=>{
+//     if (resposta == '1'){
+//         cadastrarProduto(produtos)
+//     }
+//     else if (resposta == '2'){
+//         criarCarrinho(produtos,carrinho)
+//     }
+//     else if (resposta == '3'){
+//         mostrarTotal(carrinho)
+//     }
+//     else if (resposta == '4'){
+//         mostrarCompras(carrinho)
+//     }
+// })
+
+// console.log('nova alteração')
+
+// Instalar e importar o express
+import express from 'express'
+import path from 'path'
+
+//Comando do path para acessar a pasta atual
+const pastaAtual = process.cwd()
+const pastaTemplate = path.join(pastaAtual,'templates')
+console.log(pastaTemplate)
+
+// inicialização do aplicativo express
+const app = express()
+// Criação de uma porta do servidor
+const porta = 5000
+
+// Comando para transformar os dados vindos de formulários para objetos javascript
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+// rota raiz
+app.get('/',(req,res) => {
+    res.sendFile(`${pastaTemplate}/index.html`)
 })
 
-function cadastrarProduto(produtos){
-    let nome = ''
-    let preco = 0
-    rl.question('Digite o nome do produto\n',(resposta) =>{
-        nome = resposta
-        rl.question('Digite o preço do produto\n',(resposta) =>{
-
-            preco = parseFloat(resposta)
-
-            let novoProduto = {nome:nome, preco:preco}
-            console.log(novoProduto)
-            produtos.push(novoProduto)
-            console.log(produtos)
-        })
-    })
-}
-
-function criarCarrinho(produtos,carrinho){
-    for(let i=0;i<produtos.length;i++){
-        console.log(`${i} - nome: ${produtos[i].nome}, preço: ${produtos[i].preco}`)
-    }
-    rl.question('Selecione o produto a ser adicionado\n',(resp) =>{
-        resp = parseInt(resp)
-        carrinho.push(produtos[resp])
-        console.log(carrinho)
-    })
-}
-
-function mostrarTotal(carrinho){
-    let total = 0
-    for(let i = 0; i<carrinho.length;i++){
-        total += carrinho[i].preco
-    }
-    console.log(`O total do carrinho é: ${total}`)
-}
-
-function mostrarCompras(carrinho){
-    console.log('Itens do carrinho:')
-    for(let i = 0; i<carrinho.length;i++){
-        console.log(`nome: ${carrinho[i].nome} preço: ${carrinho[i].preco}`)
-    }   
-    mostrarTotal(carrinho)
-}
-
-
-let produtos = [
-    {nome:'Notebook', preco:2500},
-    {nome:'Monitor', preco:600},
-    {nome:'Memória', preco:200},
-    {nome:'Placa de vídeo',preco:650}
-]
-
-let carrinho = [
-    {nome:'Memória', preco:200},
-    {nome:'Monitor', preco:600}
-]
-
-
-rl.question('1 - Cadastrar produto\n2 - Criar Carrinho\n3 - Mostrar total\n4 - Exibir Resumo da compra\n',(resposta)=>{
-    if (resposta == '1'){
-        cadastrarProduto(produtos)
-    }
-    else if (resposta == '2'){
-        criarCarrinho(produtos,carrinho)
-    }
-    else if (resposta == '3'){
-        mostrarTotal(carrinho)
-    }
-    else if (resposta == '4'){
-        mostrarCompras(carrinho)
-    }
+app.listen(porta, ()=>{
+    console.log(`Servidor iniciado na porta ${porta}`)
 })
-
-console.log('nova alteração')
 
 
 
